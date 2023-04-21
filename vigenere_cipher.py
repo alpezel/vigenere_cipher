a02474dda37a88dddeee796e83f2e5e19220b139
alpezel <allistairevillaruel@gmail.com>
2023-04-21 18:33:55 +0800

    pseudocode

---
+++ vigenere_cipher.py
@@ -0,0 +1,6 @@
+# ask the user for input message and key
+# convert message and key to lists of numbers 0-25 since A-Z is in 65-95 using ord(c)
+# repeating the key until its the same length of the message
+# encrypt by adding message and key, take the result and use mod 26
+# convert ciphertext numbers to letters
+# print the output
