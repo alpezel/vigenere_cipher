af39ea8a83cf8b7e4a06d6eea35435f7dc7d2ee7
alpezel <allistairevillaruel@gmail.com>
2023-04-21 19:21:05 +0800

    repeat key until its the same length of message
    using for loop and append to key_rep list

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -5,6 +5,11 @@ key = input("KEYWORD (input in uppercase letters, no spaces): ")
 message_num = [ord(c) - 65 for c in message]
 key_num = [ord(c) - 65 for c in key]
 # repeating the key until its the same length of the message
+key_rep = []
+key_index = 0
+for i in range(len(message)):
+    key_rep.append(key_num[key_index])
+    key_index = (key_index + 1) % len(key)
 # encrypt by adding message and key, take the result and use mod 26
 # convert ciphertext numbers to letters
 # print the output
