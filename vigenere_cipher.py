59e2cbaa463382eb986813a945ad9da23a128fc2
alpezel <allistairevillaruel@gmail.com>
2023-04-21 18:50:12 +0800

    user input
    message and keyword

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -1,4 +1,6 @@
 # ask the user for input message and key
+message = input("MESSAGE (input in uppercase letters, no spaces): ")
+key = input("KEYWORD (input in uppercase letters, no spaces): ")
 # convert message and key to lists of numbers 0-25 since A-Z is in 65-95 using ord(c)
 # repeating the key until its the same length of the message
 # encrypt by adding message and key, take the result and use mod 26
